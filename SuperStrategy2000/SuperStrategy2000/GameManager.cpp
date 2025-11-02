a027e015c7c56ca759878837942fb582fe967356
AnJello <anjello.30bohol@gmail.com>
2025-11-02 19:20:34 +1300

    GameManager Singleton Design Pattern
    Created the GameManager using a Singleton Design Pattern. This means only one instance of GameManager exists and can be accessed globally.

---
+++ SuperStrategy2000/SuperStrategy2000/GameManager.cpp
@@ -0,0 +1,21 @@
+#include "GameManager.h"
+
+// Define the Static GameManager Instance
+GameManager* GameManager::m_instance = nullptr;
+
+GameManager::GameManager() {
+}
+
+GameManager::~GameManager() {
+}
+
+GameManager* GameManager::getInstance() {
+    // If there is no Instance
+    if (m_instance == nullptr) {
+        // Create an Instance
+        m_instance = new GameManager();
+    }
+
+    // Return the Singleton Instance
+    return m_instance;
+}
---
+++ SuperStrategy2000/SuperStrategy2000/GameManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "EventManager.h"
+
+class GameManager {
+private:
+	static GameManager* m_instance;
+
+	// Private Constructor and Destructor
+	GameManager(); // Constructor
+	~GameManager(); // Destructor
+
+	// Deletes the Copy Consturctor and the Assignment Operator
+	GameManager(const GameManager&) = delete; // Copy Constructor
+	GameManager& operator=(const GameManager&) = delete; // Assignment Operator
+
+public:
+	// Getting Singleton Instance
+	static GameManager* getInstance(); // Return the Singleton Instance
+};
+
