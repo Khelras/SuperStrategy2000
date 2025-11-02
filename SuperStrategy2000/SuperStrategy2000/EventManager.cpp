b96fb3e16d3bc3b55f2f2557010b9fd4340e35d5
AnJello <anjello.30bohol@gmail.com>
2025-11-02 15:50:46 +1300

    MDS Header to the EventManager Files

--- SuperStrategy2000/SuperStrategy2000/EventManager.cpp
+++ SuperStrategy2000/SuperStrategy2000/EventManager.cpp
@@ -1,1 +1,13 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : EventManager.cpp
+Description : Defines the EventManager Class Functions and Properties.
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
 #include "EventManager.h"
--- SuperStrategy2000/SuperStrategy2000/EventManager.h
+++ SuperStrategy2000/SuperStrategy2000/EventManager.h
@@ -1,4 +1,18 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : EventManager.h
+Description : Declares the EventManager Class Functions and Properties. Uses a Singleton Design Pattern.
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
 #pragma once
+
 class EventManager {
+
 };
 
