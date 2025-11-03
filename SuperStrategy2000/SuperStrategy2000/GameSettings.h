e14219c4ed708995cf16659df32e0bfac6a3c878
AnJello <anjello.30bohol@gmail.com>
2025-11-03 21:29:02 +1300

    GameSettings Singleton Design Pattern
    Created the basic GameSettings struct which follows a Singleton Design Pattern. This ensures only one instance of GameSettings exist and that all values in GameSettings can be accessed globally.

---
+++ SuperStrategy2000/SuperStrategy2000/GameSettings.h
@@ -0,0 +1,46 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : GameSettings.h
+Description : Holds all the Game Settings information. Follows a Singleton Design Pattern
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+#pragma once
+
+struct GameSettings {
+private:
+	// Singleton Instance
+	static GameSettings* m_instance;
+
+	// Private Constructor
+	GameSettings() {}; 
+
+	// Delete Copy Constructor and Assignment
+	GameSettings(GameSettings const&) = delete;
+	void operator=(GameSettings const&) = delete;
+
+public:
+	// Getting the Singleton Instance
+	static GameSettings* getInstance() {
+		// If there is no Instance
+		if (m_instance == nullptr) {
+			// Create an Instance
+			m_instance = new GameSettings();
+		}
+
+		// Return the Singleton Instance
+		return m_instance;
+	}
+
+	// Game Settings Values
+	int m_windowX = 0; // Width of Window
+	int m_windowY = 0; // Height of Window
+	bool m_vsync = false; // Enable v-sync?
+	float m_volumn = 50.0f; // Volumn of the Game
+};
+
