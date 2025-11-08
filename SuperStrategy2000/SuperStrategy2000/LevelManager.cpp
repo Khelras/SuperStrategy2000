686f6f0a2be5303e6bdcdd0fa9497fafa806ee46
AnJello <anjello.30bohol@gmail.com>
2025-11-09 01:50:02 +1300

    Initial Functions and Properties
    Added the initial functions and properties in LevelManager.
    - Added m_levels member variable.
    - Added m_completedLevels member variable.
    - Added m_currentLevel member variable.
    - Added a basic constructor to allow LevelManager to be instantiated.
    - Added destructor which deletes all the levels.
    - Pre-Added loadLevel() public function.
    - Pre-Added saveLevel() public function.
    - Added selectLevel() public function.
    - Added findLevel() private function.

--- SuperStrategy2000/SuperStrategy2000/LevelManager.cpp
+++ SuperStrategy2000/SuperStrategy2000/LevelManager.cpp
@@ -12,3 +12,63 @@ Mail        : angelo.bohol@mds.ac.nz
 
 #include "LevelManager.h"
 #include "GameManager.h"
+
+LevelManager::LevelManager() {
+	this->m_currentLevel = nullptr;
+}
+
+LevelManager::~LevelManager() {
+	// Loop through the Levels
+	for (auto& level : this->m_levels) {
+		// Ensure Level exists
+		if (level != nullptr) {
+			// Delete Level
+			delete(level);
+			level = nullptr;
+		}
+	}
+}
+
+void LevelManager::process() {
+
+}
+
+void LevelManager::loadLevel(std::string _path) {
+	// TODO: MAKE LEVEL LOADING
+}
+
+void LevelManager::saveLevel(std::string _path) {
+	// TODO: MAKE LEVEL SAVING
+}
+
+void LevelManager::selectLevel(int _levelNumber) {
+	// Level
+	Level* level = this->findLevel(_levelNumber);
+
+	// Level Exists
+	if (level != nullptr) {
+		this->m_currentLevel = level;
+	}
+	// Level does NOT Exists
+	else if (level == nullptr) {
+		std::cerr << "Unknown Level..?" << std::endl;
+	}
+}
+
+Level* LevelManager::findLevel(int _levelNumber) {
+	// Result
+	Level* result = nullptr; // Default to nullptr
+
+	// Loop through the List of Levels
+	for (auto& level : this->m_levels) {
+		// Check the Level Number
+		if (level->m_levelNumber == _levelNumber) {
+			// Update Result
+			result = level;
+			return; // End Loop
+		}
+	}
+
+	// Return the Result
+	return result;
+}
--- SuperStrategy2000/SuperStrategy2000/LevelManager.h
+++ SuperStrategy2000/SuperStrategy2000/LevelManager.h
@@ -1,16 +1,27 @@
-/***********************************************************************
-Bachelor of Software Engineering
-Media Design School
-Auckland
-New Zealand
-(c) 2025 Media Design School
-File Name   : LevelManager.h
-Description : Declares the LevelManager Class Functions and Properties.
-Author      : Angelo Joseph Arawiran Bohol
-Mail        : angelo.bohol@mds.ac.nz
-**************************************************************************/
+
 
 #pragma once
+#include "Level.h"
+
 class LevelManager {
+public:
+	// Levels
+	std::vector<Level*> m_levels; // All Levels
+	std::vector<Level*> m_completedLevels; // Completed Levels
+	Level* m_currentLevel; // Current Level
+
+	// Constructor and Destructor
+	LevelManager();
+	~LevelManager();
+
+	// Level Manager Functions and Properties
+	void process(); // Level Manager Process Loop
+	void loadLevel(std::string _path); // Load Level
+	void saveLevel(std::string _path); // Save Level
+	void selectLevel(int _levelNumber); // Select Level
+
+private:
+	// Private Functions
+	Level* findLevel(int _levelNumber); // Returns a Level given the Level Number
 };
 
