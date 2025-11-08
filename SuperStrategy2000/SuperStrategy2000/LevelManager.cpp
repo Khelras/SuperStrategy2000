2028aed7fb0c2998a333ad3d5a672cf9b5daa472
AnJello <anjello.30bohol@gmail.com>
2025-11-09 01:20:33 +1300

    Initial Level Manager
    Created the LevelManager ".h" and ".cpp" files

---
+++ SuperStrategy2000/SuperStrategy2000/LevelManager.cpp
@@ -0,0 +1,14 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : LevelManager.cpp
+Description : Defines the LevelManager Class Functions and Properties.
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+#include "LevelManager.h"
+#include "GameManager.h"
---
+++ SuperStrategy2000/SuperStrategy2000/LevelManager.h
@@ -0,0 +1,16 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : LevelManager.h
+Description : Declares the LevelManager Class Functions and Properties.
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+#pragma once
+class LevelManager {
+};
+
