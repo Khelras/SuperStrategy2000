3674c5ca77faa51675ae6872916b3a5ec07e7b25
AnJello <anjello.30bohol@gmail.com>
2025-11-09 20:17:31 +1300

    Initial Sound Manager
    Created the initial SoundManager Class.
    - Added a background.wav file for testing.

---
+++ SuperStrategy2000/SuperStrategy2000/SoundManager.cpp
@@ -0,0 +1,13 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : SoundManager.cpp
+Description : Defines the SoundManager Class Functions and Properties.
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+#include "SoundManager.h"
