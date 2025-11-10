b9daaab7a45ff2fd4a67aa5a54c9b41ae851fe15
AnJello <anjello.30bohol@gmail.com>
2025-11-11 12:30:15 +1300

    Base Actor Class
    Created a Base Actor Class which will be an abstract class for other Actor classes to derive from.
    - Added the initiall enum actor types which will help with identifing different Actors as well as downcasting for their functionality.

---
+++ SuperStrategy2000/SuperStrategy2000/Actor.cpp
@@ -0,0 +1,15 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : Actor.cpp
+Description : Defines the Actor Class Functions and Properties
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+#include "Actor.h"
+#include "GameManager.h"
+#include "GameSettings.h"
---
+++ SuperStrategy2000/SuperStrategy2000/Actor.h
@@ -0,0 +1,27 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : Actor.h
+Description : Declares the Actor Class Functions and Properties
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+
+
+#pragma once
+class Actor {
+public:
+	// The Different Types of Actors
+	enum class Type {
+		NONE = 0,
+		OBSTACLE = 1,
+		UNIT = 2,
+	};
+	
+	
+};
+
