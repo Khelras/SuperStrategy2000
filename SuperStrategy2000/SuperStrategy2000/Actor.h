eb81c7fa4188ee7f42cefa1124b72326e6a8d849
AnJello <anjello.30bohol@gmail.com>
2025-11-11 13:15:21 +1300

    Inital Base Actor
    Completed the intial base Actor which holds position and the sprite of the Actor.

--- SuperStrategy2000/SuperStrategy2000/Actor.h
+++ SuperStrategy2000/SuperStrategy2000/Actor.h
@@ -5,15 +5,20 @@ Auckland
 New Zealand
 (c) 2025 Media Design School
 File Name   : Actor.h
-Description : Declares the Actor Class Functions and Properties
+Description : Declares the Actor Abstract Class Functions and Properties
 Author      : Angelo Joseph Arawiran Bohol
 Mail        : angelo.bohol@mds.ac.nz
 **************************************************************************/
 
-
+#include <SFML/Graphics.hpp>
 
 #pragma once
 class Actor {
+protected:
+	// Actor Attributes
+	sf::Vector2f m_actorPosition;
+	sf::Sprite* m_actorSprite;
+
 public:
 	// The Different Types of Actors
 	enum class Type {
@@ -22,6 +27,17 @@ public:
 		UNIT = 2,
 	};
 	
-	
+	// Constructor and Destructor
+	Actor(); // Default Constructor
+	Actor(sf::Vector2f _position); // Constructor
+	virtual ~Actor(); // Virtual Destructor
+
+	// Virtual Actor Position Functions
+	virtual void setActorPosition(sf::Vector2f _position); // Sets the Actor Position
+	virtual const sf::Vector2f& getActorPosition() const; // Returns the Actor Position
+
+	// Virtual Actor Sprite Functions
+	virtual void setActorSprite(sf::Vector2u _index); // Sets the Actor Sprite
+	virtual const sf::Sprite* getActorSprite() const // Returns the Actor Sprite
 };
 
