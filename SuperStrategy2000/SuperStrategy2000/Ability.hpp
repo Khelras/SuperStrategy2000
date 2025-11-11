02e33ca8a356d2c86f2e76837cfb0afd0334cba3
AnJello <anjello.30bohol@gmail.com>
2025-11-11 18:53:13 +1300

    Added Ability Cooldowns
    Abilities can now be on cooldown which basically means they can only be used again after a certain amount of turns has passed.

--- SuperStrategy2000/SuperStrategy2000/Ability.hpp
+++ SuperStrategy2000/SuperStrategy2000/Ability.hpp
@@ -5,7 +5,7 @@ Auckland
 New Zealand
 (c) 2025 Media Design School
 File Name   : Ability.hpp
-Description : Declares the Ability Abstract Class Functions and Properties.
+Description : Declares and Defines the Ability Abstract Class Functions and Properties.
 Author      : Angelo Joseph Arawiran Bohol
 Mail        : angelo.bohol@mds.ac.nz
 **************************************************************************/
@@ -18,17 +18,30 @@ protected:
 	// Ability Attributes
 	std::string m_abilityName;
 	std::string m_abilityDescription;
+	unsigned int m_abilityCooldown = 0;
+	unsigned int m_abilityCurrentCooldown = 0;
 
 public:
 	// Constructor and Destructor
 	Ability() {}; // Constructor
 	virtual ~Ability() {}; // Virtual Destructor
 
-	// Ability Functions
+	// Ability Execute Function
 	virtual void execute(Unit* _user, Unit* _target) const = 0;
 
 	// Getters
 	const std::string& getAbilityName() const { return this->m_abilityName; };
 	const std::string& getAbilityDescription() const { return this->m_abilityDescription; };
+	const unsigned int& getAbilityCooldown() const { return this->m_abilityCooldown; };
+	const unsigned int& getAbilityCurrentCooldown() const { return this->m_abilityCurrentCooldown; };
+
+	// Update Cooldown
+	void updateCooldown() {
+		// Check if Current Cooldown is lesser than Cooldown
+		if (this->m_abilityCurrentCooldown < this->m_abilityCooldown) {
+			// Increase Current Cooldown
+			this->m_abilityCurrentCooldown++;
+		}
+	};
 };
 
