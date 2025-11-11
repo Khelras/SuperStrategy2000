377921751b6925f9349b029da03373d0fdf705bf
AnJello <anjello.30bohol@gmail.com>
2025-11-11 14:41:32 +1300

    TileMap System
    Created the a TileMap system which handles loading a Tile Set into a Texture from a path. The TileMap system is responsible for section the loaded Tile Set Texture into different little Tiles based on the size of each Tiles. This would be memory efficient since there is only a singular loaded Tile Set.
    
    Properties:
    - Added m_tileMapTexture.
    - Added m_errorTexture.
    - Added m_tileSize.
    - Added m_tileMapSize.
    - Added m_tileMapSizeInTiles.
    - Added m_totalTiles.
    - Added m_hasLoadedTexture.
    
    Functions:
    - Added TileMap Constructors.
    - Added TileMap Destructor.
    - Added loadTileMap().
    - Added getTile().
    - Added getTileMapTexture().
    - Added getTileSize().
    - Added getTileMapSize().
    - Added getTileMapSizeInTiles().
    - Added getTotalTiles().
    - Added assertXY().

--- SuperStrategy2000/SuperStrategy2000/TileMap.cpp
+++ SuperStrategy2000/SuperStrategy2000/TileMap.cpp
@@ -13,3 +13,140 @@ Mail        : angelo.bohol@mds.ac.nz
 #include "TileMap.h"
 #include "GameManager.h"
 #include "GameSettings.h"
+
+TileMap::TileMap() {
+	// Load the Test Tile Map
+	this->loadTileMap("assets/tilesets/test.png", 32);
+
+	// Error Tile Texture
+	sf::Image redImage({ 16, 16 }, sf::Color::Red); // Creates a Red 16x16 Image
+	if (this->m_errorTexture.loadFromImage(redImage)); // Loads the Texture from the Red 16x16 Image
+}
+
+TileMap::TileMap(std::string _path, int _tileSides) {
+	// Load Tile Map
+	this->loadTileMap(_path, _tileSides, _tileSides);
+
+	// Error Tile Texture
+	sf::Image RedImage({ 16, 16 }, sf::Color::Red); // Creates a Red 16x16 Image
+	if (this->m_errorTexture.loadFromImage(RedImage)); // Loads the Texture from the Red 16x16 Image
+}
+
+TileMap::TileMap(std::string _path, int _tileWidth, int _tileHeight) {
+	// Load Tile Map
+	this->loadTileMap(_path, _tileWidth, _tileHeight);
+
+	// Error Tile Texture
+	sf::Image RedImage({ 16, 16 }, sf::Color::Red); // Creates a Red 16x16 Image
+	if (this->m_errorTexture.loadFromImage(RedImage)); // Loads the Texture from the Red 16x16 Image
+}
+
+TileMap::~TileMap() {
+}
+
+bool TileMap::loadTileMap(std::string _path, unsigned int _tileSides) {
+	// Loading was Successful
+	if (this->loadTileMap(_path, _tileSides, _tileSides) == true) {
+		return true;
+	}
+
+	// Returns false otherwise (File Loading Failed)
+	return false;
+}
+
+bool TileMap::loadTileMap(std::string _path, unsigned int _tileWidth, unsigned int _tileHeight) {
+	// Validate Tile Map Texture successfully loads from File Path
+	if (this->m_tileMapTexture.loadFromFile(_path)) {
+		// Tile Map Attributes
+		this->m_tileSize.x = _tileWidth;
+		this->m_tileSize.y = _tileHeight;
+		this->m_tileMapSize.x = this->m_tileMapTexture.getSize().x;
+		this->m_tileMapSize.y = this->m_tileMapTexture.getSize().y;
+		this->m_tileMapSizeinTiles.x = this->m_tileMapTexture.getSize().x / _tileWidth;
+		this->m_tileMapSizeinTiles.y = this->m_tileMapTexture.getSize().y / _tileHeight;
+		this->m_totalTiles = this->m_tileMapSizeinTiles.x * this->m_tileMapSizeinTiles.y;
+
+		// Loading Successful
+		this->m_hasLoadedTexture = true;
+		return true;
+	}
+
+	// Returns false otherwise (File Loading Failed)
+	return false;
+}
+
+sf::IntRect TileMap::getTile(unsigned int _x, unsigned int _y) const {
+	// First check if a Tile Map has loaded and the given X and Y values are valid
+	if (this->m_hasLoadedTexture && this->assertXY(_x, _y)) {
+		// Return a FloatRect Section on the Tile Map
+		return sf::IntRect(
+			sf::Vector2i( // Position
+				_x * static_cast<int>(this->m_tileSize.x),
+				_y * static_cast<int>(this->m_tileSize.y)
+			),
+			sf::Vector2i( // Size
+				static_cast<int>(this->m_tileMapSize.x),
+				static_cast<int>(this->m_tileMapSize.y)
+			)
+		);
+	}
+	// There is no Loaded Texture
+	else {
+		// Return the Default
+		return sf::IntRect(
+			sf::Vector2i( // Position
+				0, 0
+			),
+			sf::Vector2i( // Size
+				static_cast<int>(this->m_tileMapSize.x),
+				static_cast<int>(this->m_tileMapSize.y)
+			)
+		);
+	}
+}
+
+sf::IntRect TileMap::getTile(unsigned int _n) const {
+	// Convert to X and Y
+	unsigned int tileX = _n % this->m_tileMapSize.x;
+	unsigned int tileY = _n / this->m_tileMapSize.x;
+
+	// Return using the GetTile() function and pass in the X and Y
+	return this->getTile(tileX, tileY);
+}
+
+const sf::Texture& TileMap::getTileMapTexture() const {
+	// If a Tile Map has been Loaded
+	if (this->m_hasLoadedTexture == true) {
+		return this->m_tileMapTexture;
+	}
+
+	// Otherwise, Return the Error Texture
+	return this->m_errorTexture;
+}
+
+sf::Vector2u TileMap::getTileSize() const {
+	return this->m_tileSize;
+}
+
+sf::Vector2u TileMap::getTileMapSize() const {
+	return this->m_tileMapSize;
+}
+
+sf::Vector2u TileMap::getTileMapSizeInTiles() const {
+	return this->m_tileMapSizeinTiles;
+}
+
+unsigned int TileMap::getTotalTiles() const {
+	return this->m_totalTiles;
+}
+
+bool TileMap::assertXY(unsigned int _x, unsigned int _y) const {
+	// Check if the given X and Y are within the Tile Map X and Y (in Terms of Tiles)
+	if (_x < this->m_tileMapSizeinTiles.x && _y < this->m_tileMapSizeinTiles.y) {
+		// Return true (Assertion successful)
+		return true;
+	}
+
+	// Otherwise, Return False (Assertion failed)
+	return false;
+}
--- SuperStrategy2000/SuperStrategy2000/TileMap.h
+++ SuperStrategy2000/SuperStrategy2000/TileMap.h
@@ -14,6 +14,38 @@ Mail        : angelo.bohol@mds.ac.nz
 #include <SFML/Graphics.hpp>
 
 class TileMap {
-	
+private:
+	// Tile Map Attributes
+	sf::Texture m_tileMapTexture; // Tile Map Texture
+	sf::Texture m_errorTexture; // Red Square Tile Texture (For Errors)
+	sf::Vector2u m_tileSize; // The Size of a Single Tile
+	sf::Vector2u m_tileMapSize; // The Size of the Tile Map
+	sf::Vector2u m_tileMapSizeinTiles; // The Size of the Tile Map in terms of Tiles
+	unsigned int m_totalTiles; // Total Number of Tiles within the Tile map
+	bool m_hasLoadedTexture; // If a Tile Map Texture has been Loaded
+
+public:
+	// Constructor and Destructor
+	TileMap(); // Default Constructor;
+	TileMap(std::string _path, int _tileSides); // Constructor given a Path and Tile Sides (Square Tiles)
+	TileMap(std::string _path, int _tileWidth, int _tileHeight); // Constructor given a Path and Tile Height and Width
+	~TileMap();
+
+	// Tile Map Functions
+	bool loadTileMap(std::string _path, unsigned int _tileSides); // Load Tile Map given a Path and Tiles Sides (Square Tiles)
+	bool loadTileMap(std::string _path, unsigned int _tileWidth, unsigned int _tileHeight); // Load Tile Map given a Path and Tile Height and Width
+	sf::IntRect getTile(unsigned int _x, unsigned int _y) const; // Get the Tile from Tile Map given the X and Y (0-Index)
+	sf::IntRect getTile(unsigned int _n) const; // Get the n'th Tile from Tile Map given N (0-Index)
+
+	// Getters
+	const sf::Texture& getTileMapTexture() const; // Returns the Tile Map Texture
+	sf::Vector2u getTileSize() const; // Returns the Size of the Tile
+	sf::Vector2u getTileMapSize() const; // Returns the Size of the Tile Map
+	sf::Vector2u getTileMapSizeInTiles() const; // Returns the Size of Tile Map in terms of Tiles
+	unsigned int getTotalTiles() const; // Returns the Total number of Tiles
+
+private:
+	// Private Helper Functions
+	bool assertXY(unsigned int _x, unsigned int _y) const; // Ensures that X and Y values were given are valid (0-Index)
 };
 
