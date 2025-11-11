247587c92eefa89d1f45e9438bcca176a6a1263b
AnJello <anjello.30bohol@gmail.com>
2025-11-11 13:57:00 +1300

    Initial TileMap
    Created the initial TileMap class.

---
+++ SuperStrategy2000/SuperStrategy2000/TileMap.cpp
@@ -0,0 +1,15 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : TileMap.cpp
+Description : Defines the TileMap Class Functions and Properties
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+#include "TileMap.h"
+#include "GameManager.h"
+#include "GameSettings.h"
---
+++ SuperStrategy2000/SuperStrategy2000/TileMap.h
@@ -0,0 +1,19 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School
+Auckland
+New Zealand
+(c) 2025 Media Design School
+File Name   : TileMap.h
+Description : Declares the TileMap Class Functions and Properties
+Author      : Angelo Joseph Arawiran Bohol
+Mail        : angelo.bohol@mds.ac.nz
+**************************************************************************/
+
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class TileMap {
+	
+};
+
